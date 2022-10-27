db47217df1f13f062aee439b42e5721aeaf0de05
Gwardzik <gwardzik.patryk@gmail.com>
2022-10-27 23:23:12 +0200

    Random accuracy resolved and tested

--- Project 3 - SpaceChallenge/src/Main.java
+++ Project 3 - SpaceChallenge/src/Main.java
@@ -3,7 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         U1 ship = new U1();
-        ship.launch();
+        int isFalse = 0;
+        int isTrue = 0;
+        for (int i = 0; i < 1000000000; i++) {
 
+            if (ship.launch()) {
+                isTrue++;
+            } else {
+                isFalse++;
+
+            }
+        }
+        System.out.println(isTrue);
+        System.out.println(isFalse);
     }
 }
--- Project 3 - SpaceChallenge/src/U1.java
+++ Project 3 - SpaceChallenge/src/U1.java
@@ -2,19 +2,17 @@ public class U1 extends Rocket {
     int rocketWeightNoCargo = 10000;
     int maxRocketWeight = 18000;
     int cargoLimit = maxRocketWeight - rocketWeightNoCargo;
-    int launchSucces = 5;
-    int accurecyCoeficiant = 1000;
+    int launchSuccess = 5;
+    int accurecyCoeficiant = 1000000;
 
 
     @Override
     public boolean launch() {
-        double losowaLiczba = (Math.random() * 100*accurecyCoeficiant)+1;
+        double randomNumber = (Math.random() * 100 * accurecyCoeficiant) + 1;
 
-        System.out.println((int)losowaLiczba);
-
-        (launchSucces*accurecyCoeficiant)*((currentRocketWeight-rocketWeightNoCargo)/cargoLimit);
-
-        return true;
+       // System.out.println((int) randomNumber);
+       // System.out.println((launchSuccess * accurecyCoeficiant) * ((currentRocketWeight - rocketWeightNoCargo) / cargoLimit));
+        return (int) randomNumber > (launchSuccess * accurecyCoeficiant) * ((currentRocketWeight - rocketWeightNoCargo) / cargoLimit);
     }
 
     @Override
