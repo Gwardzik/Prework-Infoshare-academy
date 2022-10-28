1378f96d112d44d15e8c9a1b1caac0ef698f6b33
Gwardzik <gwardzik.patryk@gmail.com>
2022-10-28 13:44:52 +0200

    parto of simulatiom has been added

--- Project 3 - SpaceChallenge/src/Main.java
+++ Project 3 - SpaceChallenge/src/Main.java
@@ -1,20 +1,17 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
 
         U1 ship = new U1();
-        int isFalse = 0;
-        int isTrue = 0;
-        for (int i = 0; i < 100000; i++) {
 
-            if (ship.launch()) {
-                isTrue++;
-            } else {
-                isFalse++;
+        Simulation simulation = new Simulation();
+        try {
+             simulation.loadItems();
 
-            }
+        } catch (FileNotFoundException notFoundException) {
+            System.out.println("FileNotFound exeptiona has been ussed");
         }
-        System.out.println(isTrue);
-        System.out.println(isFalse);
     }
 }
--- Project 3 - SpaceChallenge/src/Simulation.java
+++ Project 3 - SpaceChallenge/src/Simulation.java
@@ -1,9 +1,32 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Simulation {
-    ArrayList loadItems(){
-        ArrayList
-        return ;
+
+    File phase1File = new File("/home/patryk/Desktop/Projekty/prework dla infoshare/Project 3 - SpaceChallenge/src/cargo files/Phase-1.txt");
+    String tempString;
+    ArrayList loadItems() throws FileNotFoundException {
+
+        ArrayList<Item> itemArrayList = new ArrayList<>();
+
+        Scanner scanner = new Scanner(phase1File);
+
+        while (scanner.hasNextLine()) {
+            Item newItem = new Item();
+            tempString = scanner.nextLine();
+            // making first substring from whole line-name of object
+            newItem.name = tempString.substring(0, tempString.indexOf("="));
+            //making second substring-weight of the oject, converting String to int thx to Integer.parseInt
+            newItem.weight = Integer.parseInt(tempString.substring(tempString.indexOf("=") + 1));
+            //adding object to ArrayList
+            itemArrayList.add(newItem);
+            System.out.println(itemArrayList);
+        }
+
+        return itemArrayList;
     }
-    
+
 }
