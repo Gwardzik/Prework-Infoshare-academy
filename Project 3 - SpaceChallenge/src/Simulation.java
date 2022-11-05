02b4102788f0cd42688af2cb3ff637d248eb0eb3
Gwardzik <gwardzik.patryk@gmail.com>
2022-11-05 21:12:04 +0100

    Bubble short updat

--- Project 3 - SpaceChallenge/src/Simulation.java
+++ Project 3 - SpaceChallenge/src/Simulation.java
@@ -56,6 +56,9 @@ public class Simulation {
             leftCheck++;
             rightCheck++;
         }
+        bubbleSort(leftCheck , itemListToShort);
+        bubbleSort((rightCheck, itemListToShort));
+
         return itemListToShort;
     }
 
